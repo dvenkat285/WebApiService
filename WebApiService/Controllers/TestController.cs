b557c9be06396296fbe60096102979909cdcec4c
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-08-21 08:38:53 +0530

    Add project files.

---
+++ WebApiService/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiService.Controllers
+{
+    public class TestController : ApiController
+    {
+        static List<String> Colors = new List<String>()
+        {
+        "Blue","White","Black","Pink","Red","Yellow","Voilet","Green","Purple","Mangenta"
+        };
+        [HttpGet]
+        public IEnumerable<String>DisplayColors()
+            {
+            return Colors; 
+            }
+        [HttpGet]
+        public String DisplayColor(int Id)
+        {
+            return Colors[Id];
+        }
+        public void Post([FromBody]string color)
+        {
+            Colors.Add(color);
+        }
+        public void Put(int Id, [FromBody] string color)
+        {
+            Colors[Id] = color;
+        }
+        public void Delete(int Id)
+        {
+            Colors.RemoveAt(Id);
+        }
+    }
+}
